8427ab5aa617b1a242965c79b15479ea058f797d
JOSEIA13 <unioncafetero@gmail.com>
2025-07-30 19:42:29 -0500

    se importo l alibreria nltk

---
+++ stopwords.py
@@ -0,0 +1,11 @@
+# import nltk
+import nltk
+
+# desde nltk descargar stopwords
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+lista_stopwords = stopwords.words('spanish')
+# lista_stopwords = stopwords.words('spanish')
+
+# imprimir la lista de stopwords
+print(lista_stopwords)
