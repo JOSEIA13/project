e70f09a80a0730f5487fb4610a84044c1e18dfa8
JOSEIA13 <unioncafetero@gmail.com>
2025-07-30 22:01:48 -0500

    Se importo nltk en el archivo main

--- stopwords.py
+++ stopwords.py
@@ -4,8 +4,9 @@ import nltk
 # desde nltk descargar stopwords
 from nltk.corpus import stopwords
 nltk.download('stopwords')
-lista_stopwords = stopwords.words('spanish')
+lista_stopwords = stopwords.words('english')
 # lista_stopwords = stopwords.words('spanish')
 
 # imprimir la lista de stopwords
 print(lista_stopwords)
+
